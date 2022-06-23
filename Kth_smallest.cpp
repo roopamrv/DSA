413e39586852a9cdf8057aab99d9aa0c785e592f
roopamrv <89948461+roopamrv@users.noreply.github.com>
2022-06-23 01:12:51 -0500

    Add files via upload

---
+++ Kth_smallest.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<iostream>
+#include<queue>
+
+
+using namespace std;
+
+void Ksmallest(vector<int> &arr,int n,int k){
+    priority_queue<int> pq;
+    for(auto &i: arr){
+        pq.push(-i);
+    }
+
+    for(int i=1;i<k;i++){
+        pq.pop();
+    }
+    cout<<-pq.top()<<endl;
+}
+
+int main(){
+    vector<int> arr{10,20,40,60,8,9,4};
+    int n = arr.size();
+    int k = 3;
+
+    Ksmallest(arr,n,k);
+}
